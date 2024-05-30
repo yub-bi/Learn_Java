6cf24b6a929f7facea9a1546c11feb0eac128280
yub-bi <hoihoi5468@gmail.com>
2024-05-30 09:34:35 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+        /**
+        * @author 굿
+         * @deprecated
+         * @apiNote 이것은 무엇을 하는 기능입니다.
+         * 자바 독
+        **/
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("good");
+        System.out.print(1);
+        System.out.println("sout");
+//        'ctrl+/'
+        /*
+        *
+        * 'ctrl+shift+/'
+        *
+        * */
+    }
+}
